87f19c331b2df24a49e65e57ba29f104201ac90f
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-19 15:24:14 -0700

    create center panel components
    Co-authored-by: Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>

---
+++ src/components/CenterPanel/CenterPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Feed } from 'semantic-ui-react';
+
+function CenterPanel () {
+  return (
+    <>
+      <StatusUpdate/>
+      <FeedManager/>
+    </>
+  )
+}
+
+export default CenterPanel;
+
+
